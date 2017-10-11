76467aaaa7b983244951c8eb03a5e035fad72b13
Unknown <jiahan.jim.liu@gmail.com>
2017-10-11 17:03:58 -0500

    Version 0.0 from Tivaware for freeRTOS

---
+++ IPGES/WiseControlSystem/rvmdk/buttons.d
@@ -0,0 +1,12 @@
+.\rvmdk\buttons.o: ..\drivers\buttons.c
+.\rvmdk\buttons.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+.\rvmdk\buttons.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\rvmdk\buttons.o: ..\inc/hw_types.h
+.\rvmdk\buttons.o: ..\inc/hw_memmap.h
+.\rvmdk\buttons.o: ..\inc/hw_gpio.h
+.\rvmdk\buttons.o: ..\driverlib/sysctl.h
+.\rvmdk\buttons.o: ..\driverlib/rom.h
+.\rvmdk\buttons.o: ..\driverlib/rom_map.h
+.\rvmdk\buttons.o: ..\driverlib/pin_map.h
+.\rvmdk\buttons.o: ..\driverlib/gpio.h
+.\rvmdk\buttons.o: ..\drivers/buttons.h
---
+++ IPGES/WiseControlSystem/rvmdk/rgb.d
@@ -0,0 +1,15 @@
+.\rvmdk\rgb.o: ..\drivers\rgb.c
+.\rvmdk\rgb.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\rvmdk\rgb.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+.\rvmdk\rgb.o: ..\inc/hw_types.h
+.\rvmdk\rgb.o: ..\inc/hw_memmap.h
+.\rvmdk\rgb.o: ..\inc/hw_timer.h
+.\rvmdk\rgb.o: ..\inc/hw_ints.h
+.\rvmdk\rgb.o: ..\driverlib/sysctl.h
+.\rvmdk\rgb.o: ..\driverlib/rom.h
+.\rvmdk\rgb.o: ..\driverlib/rom_map.h
+.\rvmdk\rgb.o: ..\driverlib/pin_map.h
+.\rvmdk\rgb.o: ..\driverlib/timer.h
+.\rvmdk\rgb.o: ..\driverlib/gpio.h
+.\rvmdk\rgb.o: ..\driverlib/interrupt.h
+.\rvmdk\rgb.o: ..\drivers\rgb.h
